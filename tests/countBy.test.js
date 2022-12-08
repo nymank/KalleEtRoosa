778a0b645ba19c380ddf8453f8259fca500c48d3
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2022-12-08 16:40:47 +0200

    update filter.test and countBy.test

--- tests/countBy.test.js
+++ tests/countBy.test.js
@@ -9,13 +9,15 @@ import countBy from '../src/countBy.js';
 const users = [
     { 'user': 'barney', 'active': true },
     { 'user': 'betty', 'active': true },
+    { 'user': 'betty', 'active': false },
+    { 'user': 'betty', 'active': false },
     { 'user': 'fred', 'active': false }
     ]
 
 describe('Count by test', function () {
     it('should count the corresponding value of each key is the number of times the key was returned', function () {
+
         const result = countBy(users, value => value.active);
-        console.log(result);
-        //expect(result).to.equal()
+        expect(result).to.deep.equal({ true: 2, false: 3 })
     });
 });
