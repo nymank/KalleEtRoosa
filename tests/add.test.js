ef6afd6c666ae24a250c899cd686961efaac1c1c
Kalle Nyman <kalle.nyman@tuni.fi>
2022-12-08 13:46:10 +0200

    first three add test cases

--- tests/add.test.js
+++ tests/add.test.js
@@ -2,17 +2,22 @@
 // source: https://www.kindacode.com/article/node-js-how-to-use-import-and-require-in-the-same-file/
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-
+//
 const { expect } = require('chai');
-const chai = require('chai');
-
 import add from '../src/add.js';
 
 
 describe('Add test', function () {
-    it('should add numbers correctly', function () {
-        const result = add(1, 2);
-        console.log(result);
-        expect(result).to.equal(3)
+    it('should add 2 + 1 => 3', function () {
+        const result = add(2, 1);
+        expect(result).to.equal(3);
+    });
+    it("should add 1 - 1 => 0", function () {
+        const result = add(1, -1);
+        expect(result).to.equal(0);
+    });
+    it("result should be a number", function () {
+        const result = add(1, -1);
+        expect(result).to.be.a("number");
     });
 });
