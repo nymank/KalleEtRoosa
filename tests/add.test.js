d636f7121c4b1dae0aa6dcd85ff8d1c1fedb1ea0
Kalle Nyman <kalle.nyman@tuni.fi>
2022-12-08 13:21:35 +0200

    hacking require

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,1 +1,18 @@
-console.log("add test");
+// These lines make "require" available 
+// source: https://www.kindacode.com/article/node-js-how-to-use-import-and-require-in-the-same-file/
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const { expect } = require('chai');
+const chai = require('chai');
+
+import add from '../src/add.js';
+
+
+describe('Add test', function () {
+    it('should add numbers correctly', function () {
+        const result = add(1, 2);
+        console.log(result);
+        expect(result).to.equal(3)
+    });
+});
