3e740a48c5eb9294d841094c69a7554f4952c18d
Kalle Nyman <kalle.nyman@tuni.fi>
2022-12-08 15:24:21 +0200

    compact tests

--- tests/compact.test.js
+++ tests/compact.test.js
@@ -7,8 +7,25 @@ const { expect } = require('chai');
 import compact from '../src/compact.js';
 
 
-describe('compact test', function () {
-    it("s", function() {
-
+describe('Compact test', function () {
+    it("should remove all falsey values", function() {
+        const result = compact([0, "Product 1", false, "Product 2", '', 3, NaN, undefined, null])
+        expect(result).to.be.an("array")
+        result.forEach((r) => {
+            expect(r).to.be.ok;
+        });
+    });
+    it("should remove all falsey values and return correct array", function() {
+        const result = compact([0, "Product 1", false, "Product 2", '', 3, NaN, undefined, null])
+        expect(result).to.be.an("array")
+        result.forEach((r) => {
+            expect(r).to.be.ok;
+        });
+        expect(result).to.equal([1, 2, 3])
+    });
+    it("should return the same array if no falsey values", function() {
+        const result = compact([1, 2, 3])
+        expect(result).to.be.an("array")
+        expect(result).to.equal([1, 2, 3])
     });
 });
