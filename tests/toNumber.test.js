ca270355145cbf2fbddc599baa382c253ad573ad
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2022-12-08 16:00:59 +0200

    add countBy, toNumer and toString test cases

---
+++ tests/toNumber.test.js
@@ -0,0 +1,25 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const { expect } = require('chai');
+const chai = require('chai');
+
+import toNumber from '../src/toNumber.js';
+
+describe('toNumber test', function () {
+    it('should convert string to number', function () {
+        const result = toNumber("3.25");
+        expect(result).to.be.a("number");
+        expect(result).to.equal(3.25);
+    });
+    it('should return NaN with null', function () {
+        const result = toNumber("null");
+        expect(result).to.be.a("number");
+        expect(result).to.be.NaN;
+    });
+        it('should return the maximum numeric value representable in JavaScript.', function () {
+            const result = toNumber(Number.MAX_VALUE);
+            //expect(result).to.be.a("number");
+            expect(result).to.equal(1.7976931348623157e+308);
+        });
+});
---
+++ tests/toString.test.js
@@ -0,0 +1,25 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const { expect } = require('chai');
+const chai = require('chai');
+
+import toString from '../src/toString.js';
+
+describe('toString test', function () {
+    it('should convert integer to string', function () {
+        const result = toString(3);
+        expect(result).to.be.a("string");
+        expect(result).to.equal("3");
+    });
+
+    it('should return empty string with null', function () {
+        const result = toString(null);
+        expect(result).to.be.a("string");
+        expect(result).to.equal('');
+    });
+    it('should convert decimal number to string', function () {
+        const result = toString(1.5);
+        expect(result).to.be.a("string");
+    });
+});
