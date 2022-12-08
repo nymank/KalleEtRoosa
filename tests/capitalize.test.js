05a577ff8bc58188418e2b48eb351a1b9d577574
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2022-12-08 14:47:16 +0200

    add the rest test files

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -11,18 +11,15 @@ import capitalize from '../src/capitalize.js';
 describe('Capitalize test', function () {
     it('first character should be upper case and the remaining lower case.', function () {
         const result = capitalize("STORE");
-        console.log(result);
         expect(result).to.equal("Store")
     });
 
     it('after numbers the remaining character should be lower case', function () {
         const result = capitalize("123 ITEM");
-        console.log(result);
         expect(result).to.equal("123 item")
     });
     it('should convert numbers to string', function () {
         const result = capitalize(22);
-        console.log(result);
         expect(result).to.be.a("string");
         //expect(result).to.equal("22");
     });
